438901eae05c09a028f05fe5dfc56f2ec7a78279
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-04-03 19:49:37 -0300

    feat: criação do menu lateral

---
+++ src/components/menuLateral/menuLateral.jsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom';
+import Login, { ToggleModal } from '../../pages/UsuarioComum/login/login';
+import IconFechar from '../../assets/iconLoginModalClose.svg';
+import Logo from '../../assets/'
+
+
+function MenuLateral(LoginPerfil, linkPerfil){
+    return(
+        <nav class="navResponsivo">
+            <ul class="lista_dashboard-responsivo">
+                <li id="icon_fechar-responsivo">
+                    <img src={IconFechar} alt='icone para fechar o menu'/>
+                    
+                </li>
+                <li>
+                    <Link>
+                        <img src="../static/img/logo-responsivo.png"/>
+                    </Link>
+                </li>
+                <li class="active-responsivo">
+                    <Link
+                    to=''>
+                        <img class="iconMenuResponsivo" src={IconFechar}/>
+                        <p>Home</p>
+                    </Link>         
+                </li>
+                <li>
+                    <Link
+                    to=''>
+                        <img class="iconMenuResponsivo" src={IconFechar}/>
+                        <p>Sobre</p>
+                    </Link>                
+                </li>
+                <li>
+                    <Link
+                    to=''>
+                        <img class="iconMenuResponsivo" src={IconFechar}/>
+                        <p>Area do Parceiro</p>
+                    </Link> 
+                </li>
+                <li>
+                    <Link
+                    to=''>
+                        <img class="iconMenuResponsivo" src={IconFechar}/>
+                        <p>{LoginPerfil}</p>
+                    </Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default MenuLateral
