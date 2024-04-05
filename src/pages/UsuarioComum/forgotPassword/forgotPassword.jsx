3f23694536938c7e95cfbc58e1556524cc2793bf
Helton Escobar <114950662+Hscobar@users.noreply.github.com>
2024-04-05 07:53:54 -0300

    edit feat: Esqueci e Alterar Senha responsivos
    Sem querer coloquei eles no branch errado... Ainda bem que n mexi em quase nenhum código

--- src/pages/UsuarioComum/forgotPassword/forgotPassword.jsx
+++ src/pages/UsuarioComum/forgotPassword/forgotPassword.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import './forgotPassword.css';
-import BoxInfo from "../../../components/boxInfo/boxInfo";
+import {BoxInfoModal} from '../../../components/boxInfo/boxInfo';
+import ModalChangePassworld from '../changePassword/changePassword'
 import FundoSenha from '../../../assets/fundoSenha.svg';
 
 function EsqueciSenha()
 {
-   
+    const [showModalChange, setModalChange] = useState(false);
+    function AbrirModalChange(event)
+    {
+     event.preventDefault(); // impedindo o form de rederecionar a página
+      setModalChange(true);
+      
+    }
     return(
         <body id="bodyDiferente">
             <div id='boxEsqueciSenha'>
                 <div>
-                   <BoxInfo title={'Esqueci minha senha'} idBox='titleBoxBranco' idDivisor='divisorBranco'></BoxInfo>
-                    <div id="forgotBody">
-                        <div className="forgotInput">
-                            <span className="nameInput">Insira o email cadastrado</span>
-                            <input type="email"/>
+                <BoxInfoModal title={'Esqueci minha senha'} idBox='titleBoxBranco' idModal='esqueciBox' idDivisor='divisorBranco'></BoxInfoModal>
+                <form id="forgotBody" onSubmit={AbrirModalChange}>
+
+                    {/*Input de email*/}
+                    <div className="forgotInput" >
+                        <span className="nameInput">Insira o email cadastrado</span>
+                        <input type="email" required/>
                         </div>
+
+                        {/*Botões de logar e ir para cadastro*/}
                         <div id="forgotButton">
-                            <button id="forgotButtonSubmit">ENVIAR</button>
-                        </div>
-                    </div> 
+                        <button id="forgotButtonSubmit" type="submit" >ENVIAR</button>
+
+                    </div>
+                </form>
+                {showModalChange &&  <ModalChangePassworld/>}
                 </div>
                 <div>
                     <img id='imagemFundo' src={FundoSenha} alt='uma imagem com uma chave numa ponta e traços com asteristico representando o campo de senha e um cadeado na outra ponta'/>
