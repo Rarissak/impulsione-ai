53fbdd3b53057b3645b7ab9ba15c782bcdc253e9
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-04-09 07:12:52 -0300

    edit feat: correção das telas alterar e esqueci senha

--- src/pages/UsuarioComum/changePassword/changePassword.jsx
+++ src/pages/UsuarioComum/changePassword/changePassword.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import './changePassword.css';
-import {BoxInfoModal} from "../../../components/boxInfo/boxInfo";
+import BoxInfo from "../../../components/boxInfo/boxInfo";
 
 
 function apenasNumeros(evt) {
@@ -24,10 +24,8 @@ function ChangePassword()
             <div id="changeCentralize" >
 
                 <div id="changeContainer">
-
                     
-                     {/*Titulo do componente login*/}
-                     <BoxInfoModal title={'Alterar Senha'} idBox={'titleBoxBranco'} idModal={'changeBox'} idDivisor={'divisorBranco'}></BoxInfoModal>
+                    <BoxInfo title={'Alterar Senha'} idBox={'titleBoxBranco'} idDivisor={'divisorBranco'}></BoxInfo>
 
                     <div id="changeBody">
 
@@ -79,3 +77,88 @@ function ChangePassword()
 }
 
 export default ChangePassword;
+
+
+// ESTAVA COM PROBLEMAS
+
+// import React,{useState} from "react";
+// import './changePassword.css';
+// import BoxInfoModal from "../../../components/boxInfo/boxInfo";
+
+
+// function apenasNumeros(evt) {
+//     // Obtém o código ASCII do caractere digitado
+//     var charCode = (evt.which) ? evt.which : evt.keyCode;
+
+//     // Verifica se o caractere digitado é uma letra
+//     if (charCode >= 65 && charCode <= 90 || charCode >= 97 && charCode <= 122) {
+//         // Cancela o evento de digitação (não insere o caractere no campo)
+//         evt.preventDefault();
+//     }
+// }
+
+// function ChangePassword()
+// {
+//     const [senha, setSenha] = useState('');
+//     const [confirmarSenha, setConfirmarSenha] = useState('');
+
+//     return(
+//         <body id="bodyDiferente"> 
+//             <div id="changeCentralize" >
+
+//                 <div id="changeContainer">
+
+                    
+//                      {/*Titulo do componente login*/}
+//                      <BoxInfoModal title={'Alterar Senha'} idBox={'titleBoxBranco'} idModal={'changeBox'} idDivisor={'divisorBranco'}></BoxInfoModal>
+
+//                     <div id="changeBody">
+
+//                         <div className="changeInput">
+//                             <span className="nameInput">Digite o código de verificação</span>
+//                             <div id="codCentralize">
+//                                 <div className="codInput">
+//                                     <input
+//                                     type="text"
+//                                     pattern="[0-9]*"
+//                                     maxLength={6}
+//                                     onKeyDown={apenasNumeros}
+//                                     />
+//                                 </div>   
+//                             </div>
+                           
+//                         </div>
+//                         <div className="changeInput">
+//                             <span className="nameInput">Digite a Nova Senha</span>
+//                             <input 
+//                                 type="password"
+//                                 value={confirmarSenha} 
+//                                 onChange={(e) => setConfirmarSenha(e.target.value)} />
+//                         </div>
+//                         <div className="changeInput">
+//                             <span className="nameInput">Reescreva a Senha</span>
+//                             <input 
+//                                 type="password"
+//                                 value={senha} 
+//                                 onChange={(e) => setSenha(e.target.value)} />
+//                             <span>
+//                                 {(senha !== '' && confirmarSenha !== '') && senha === confirmarSenha && (
+//                                     <span className="validInput">As senhas são compatíveis!</span>
+//                                 )}
+//                             </span>
+//                         </div>
+
+//                         <div id="changeButton">
+//                             <button id="changeButtonSubmit">SALVAR</button>
+//                         </div>
+//                     </div>
+                    
+//                 </div>
+
+//             </div>
+//         </body>
+    
+//     );
+// }
+
+// export default ChangePassword;
--- src/pages/UsuarioComum/forgotPassword/forgotPassword.jsx
+++ src/pages/UsuarioComum/forgotPassword/forgotPassword.jsx
@@ -1,38 +1,28 @@
 import React, {useState} from "react";
 import './forgotPassword.css';
-import {BoxInfoModal} from '../../../components/boxInfo/boxInfo';
+import BoxInfo from '../../../components/boxInfo/boxInfo';
 import ModalChangePassworld from '../changePassword/changePassword'
 import FundoSenha from '../../../assets/fundoSenha.svg';
 
 function EsqueciSenha()
 {
-    const [showModalChange, setModalChange] = useState(false);
-    function AbrirModalChange(event)
-    {
-     event.preventDefault(); // impedindo o form de rederecionar a página
-      setModalChange(true);
-      
-    }
     return(
         <body id="bodyDiferente">
             <div id='boxEsqueciSenha'>
                 <div>
-                <BoxInfoModal title={'Esqueci minha senha'} idBox='titleBoxBranco' idModal='esqueciBox' idDivisor='divisorBranco'></BoxInfoModal>
-                <form id="forgotBody" onSubmit={AbrirModalChange}>
+                <BoxInfo title={'Esqueci minha senha'} idBox='titleBoxBranco' idDivisor='divisorBranco'></BoxInfo>
 
-                    {/*Input de email*/}
+                <form id="forgotBody">
+
                     <div className="forgotInput" >
                         <span className="nameInput">Insira o email cadastrado</span>
                         <input type="email" required/>
                         </div>
 
-                        {/*Botões de logar e ir para cadastro*/}
                         <div id="forgotButton">
                         <button id="forgotButtonSubmit" type="submit" >ENVIAR</button>
-
                     </div>
                 </form>
-                {showModalChange &&  <ModalChangePassworld/>}
                 </div>
                 <div>
                     <img id='imagemFundo' src={FundoSenha} alt='uma imagem com uma chave numa ponta e traços com asteristico representando o campo de senha e um cadeado na outra ponta'/>
@@ -43,3 +33,50 @@ function EsqueciSenha()
 }
 
 export default EsqueciSenha;
+
+
+// ESTÁ ABRINDO A TELA DE ALTERAR SENHA EM BAIXO
+
+// import React, {useState} from "react";
+// import './forgotPassword.css';
+// import BoxInfo from '../../../components/boxInfo/boxInfo';
+// import ModalChangePassworld from '../changePassword/changePassword'
+// import FundoSenha from '../../../assets/fundoSenha.svg';
+
+// function EsqueciSenha()
+// {
+//     const [showModalChange, setModalChange] = useState(false);
+//     function AbrirModalChange(event)
+//     {
+//      event.preventDefault(); // impedindo o form de rederecionar a página
+//       setModalChange(true);
+      
+//     }
+//     return(
+//         <body id="bodyDiferente">
+//             <div id='boxEsqueciSenha'>
+//                 <div>
+//                 <BoxInfo title={'Esqueci minha senha'} idBox='titleBoxBranco' idModal='esqueciBox' idDivisor='divisorBranco'></BoxInfo>
+//                 <form id="forgotBody" onSubmit={AbrirModalChange}>
+
+//                     <div className="forgotInput" >
+//                         <span className="nameInput">Insira o email cadastrado</span>
+//                         <input type="email" required/>
+//                         </div>
+
+//                         <div id="forgotButton">
+//                         <button id="forgotButtonSubmit" type="submit" >ENVIAR</button>
+
+//                     </div>
+//                 </form>
+//                 {showModalChange &&  <ModalChangePassworld/>}
+//                 </div>
+//                 <div>
+//                     <img id='imagemFundo' src={FundoSenha} alt='uma imagem com uma chave numa ponta e traços com asteristico representando o campo de senha e um cadeado na outra ponta'/>
+//                 </div>
+//             </div>
+//         </body>
+//     );
+// }
+
+// export default EsqueciSenha;
