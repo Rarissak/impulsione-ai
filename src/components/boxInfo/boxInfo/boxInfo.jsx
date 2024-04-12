b4e8e7e8fb2979d00a2310125961609a0c3f8889
Helton Escobar <114950662+Hscobar@users.noreply.github.com>
2024-04-12 06:53:04 -0300

    Feat: Atualização Css do titulo do modal

--- src/components/boxInfo/boxInfo/boxInfo.jsx
+++ src/components/boxInfo/boxInfo/boxInfo.jsx
@@ -1,5 +1,5 @@
 import './boxInfo.css';
-import '../../index.css';
+import '../../../index.css';
 // import GrowIcon from '../../assets/growIcon.svg';
 // import StoreIcon from '../../assets/storeIcon.svg';
 // import PuzzleIcon from '../../assets/puzzleIcon.svg'; 
