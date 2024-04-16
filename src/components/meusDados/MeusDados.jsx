2964b9befa5999dd404736888c18a9b3abbfb578
Helton Escobar <114950662+Hscobar@users.noreply.github.com>
2024-04-15 21:28:38 -0300

    editFeat: adicionando modal para alterar dados
    Não mexi nos planos.

--- src/components/meusDados/MeusDados.jsx
+++ src/components/meusDados/MeusDados.jsx
@@ -1,86 +1,223 @@
-import React, { useState, useEffect } from "react";
+import React, {useEffect} from "react";
 import './meusDados.css';
-import BoxInfo from '../../components/boxInfo/boxInfo';
 import InstagramIcon from '../../assets/instagramRoxo.svg';
 import FacebookIcon from '../../assets/facebookRoxo.svg';
 import ProfileImg from '../../assets/gato.webp';
- /*Função de validação de formatação do contéudo do input CPF
- Vai verificar se o cpf segue o padrão: 123.123.123-12 ou vai ter apenas 11 numeros, tem retorno boleano*/
-function isValidCPF(cpf) {
-  
-    const cpfRegex = /^(\d{3}\.\d{3}\.\d{3}-\d{2}|\d{11})$/;
-    return cpfRegex.test(cpf);
-}
 
-// Função de validação de email, vai verificar a formatação do input do campo email, tem retorno boleano
-function isValidEmail(email){
-    // Expressão regular para validar e-mail simples
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
+import EditarDadosEmp, {ToggleModal, PegandoDadosAtualizados} from '../../pages/UsuarioComum/editarDadosEmp/editarDadosEmp.jsx';
 
-// Função responsável por mostrar a mensagem de cadastro realizado com sucesso.
-function CadastroRealizado()
+// Função que vai pegar todos os dados  dos 'campos' do componente meus dados
+export function PegandoDados()
 {
-    return alert("Seu cadastro foi enviado com sucesso. Por favor aguarde a validação da conta via o e-mail cadastrado!");
+    
+    try {
+         // Pega dados
+         const elementoNome = document.getElementById("campoNome");
+         const elementoDataNascimento = document.getElementById("campoDataNascimento");
+         // const elementoFotoPerfil = document.getElementById("fotoPerfil");
+         const elementoCPF = document.getElementById("campoCPF");
+         const elementoEndereco = document.getElementById("campoEndereco");
+         const elementoNomeNegocio = document.getElementById("campoNomeNegocio");
+         const elementoNicho = document.getElementById("campoNicho");
+         const elementoModalidade = document.getElementById("campoModalidade");
+         const elementoSite = document.getElementById("campoSite");
+         const elementoMEI = document.getElementById("campoMEI");
+         const elementoEmail = document.getElementById("campoEmail");
+         const elementoTelefone = document.getElementById("campoTel");
+         const elementoInstagram = document.getElementById("campoInsta");
+         const elementoFacebook = document.getElementById("campoFace");
+         const elementoImg = document.getElementById("campoFace");
+         let nomeCompleto = null;
+         let dataNascimento = null;
+         // let imgPath = null;
+         let cpf = null;
+         let enderecoCompleto = null;
+         let nomeNegocio = null;
+         let nicho = null;
+         let modalidade = null;
+         let site = null;
+         let mei = null;
+         let email = null;
+         let telefone = null;
+         let instagram = null;
+         let facebook = null;
+         
+         // Verifica se o elemento está presente antes de acessar a propriedade textContent
+         if (elementoNome) {
+             nomeCompleto = elementoNome.textContent;
+         }
+         
+         if (elementoDataNascimento) {
+             dataNascimento = elementoDataNascimento.textContent;
+         }
+         
+         // Repita o mesmo para os outros elementos...
+         
+         // Exemplo:
+         if (elementoCPF) {
+             cpf = elementoCPF.textContent;
+         }
+         
+         if (elementoEndereco) {
+             enderecoCompleto = elementoEndereco.textContent;
+         }
+         if (elementoNomeNegocio) {
+            nomeNegocio = elementoNomeNegocio.textContent;
+        }
+        
+        if (elementoNicho) {
+            nicho = elementoNicho.textContent;
+        }
+        
+        if (elementoModalidade) {
+            modalidade = elementoModalidade.textContent;
+        }
+        
+        if (elementoSite) {
+            site = elementoSite.textContent;
+        }
+        
+        if (elementoMEI) {
+            mei = elementoMEI.textContent;
+        }
+        
+        if (elementoEmail) {
+            email = elementoEmail.textContent;
+        }
+        
+        if (elementoTelefone) {
+            telefone = elementoTelefone.textContent;
+        }
+        
+        if (elementoInstagram) {
+            instagram = elementoInstagram.textContent;
+        }
+        
+        if (elementoFacebook) {
+            facebook = elementoFacebook.textContent;
+        }
+
+    // Bota no json
+    const json = {
+        "nome-completo": nomeCompleto,
+        "data-nascimento": dataNascimento,
+      //  "img-path": imgPath,
+        "cpf": cpf,
+        "mei": mei,
+        "endereco-completo": enderecoCompleto,
+        "nome-negocio": nomeNegocio,
+        "nicho": nicho,
+        "modalidade": modalidade,
+        "site": site,
+        "email": email,
+        "telefone": telefone,
+        "perfil-instagram": instagram,
+        "perfil-facebook": facebook
+    }
+
+   
+    return json;
+    // retorna o json
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+    
+
+} 
+
+// Função que deixa a primeira letra da palavra em maiúsculo.
+function capitalizarPrimeiraLetra(string) {
+    return string.charAt(0).toUpperCase() + string.substring(1);
 }
 
-// Função que faz com que o campo recuse o input de letras no input. A função é colocada no evento onKeyDown -> ao pressionar a tecla.
-function apenasNumeros(evt) {
-    // Obtém o código ASCII do caractere digitado
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
+// Função que Vai atualizar os dados exibidos No componente MeusDados
+export function MostrarDadosAtualizados() {
+    try {
+        const testeDados = PegandoDadosAtualizados();
+        
 
-    // Verifica se o caractere digitado é uma letra
-    let letrasMaiusculas = charCode >= 65 && charCode <= 90;
-    let letrasMinusculas = charCode >= 97 && charCode <= 122;
-    if ( letrasMinusculas|| letrasMaiusculas) {
-        // Cancela o evento de digitação (não insere o caractere no campo)
-        evt.preventDefault();
+        const elementoNome = document.getElementById("campoNome");
+        const elementoDataNascimento = document.getElementById("campoDataNascimento");
+        const elementoFotoPerfil = document.getElementById("fotoPerfil");
+        const elementoCPF = document.getElementById("campoCPF");
+        const elementoEndereco = document.getElementById("campoEndereco");
+        const elementoNomeNegocio = document.getElementById("campoNomeNegocio");
+        const elementoNicho = document.getElementById("campoNicho");
+        const elementoModalidade = document.getElementById("campoModalidade");
+        const elementoSite = document.getElementById("campoSite");
+        const elementoMEI = document.getElementById("campoMEI");
+        const elementoEmail = document.getElementById("campoEmail");
+        const elementoTelefone = document.getElementById("campoTel");
+        const elementoInstagram = document.getElementById("campoInsta");
+        const elementoFacebook = document.getElementById("campoFace");
+        
+        if (elementoFotoPerfil !== null) {
+            elementoFotoPerfil.src = testeDados["foto-perfil"];
+        }
+        if (elementoNome !== null) {
+            elementoNome.textContent = testeDados["nome-completo"];
+        }
+        if (elementoDataNascimento !== null) {
+            elementoDataNascimento.textContent = testeDados["data-nascimento"];
+        }
+        if (elementoCPF !== null) {
+            elementoCPF.textContent = testeDados["cpf"];
+        }
+        if (elementoMEI !== null) {
+            elementoMEI.textContent = testeDados.mei;
+        }
+        if (elementoEndereco !== null) {
+            elementoEndereco.textContent = testeDados["endereco-completo"];
+        }
+        if (elementoEmail !== null) {
+            elementoEmail.textContent = testeDados.email;
+        }
+        if (elementoNomeNegocio !== null) {
+            elementoNomeNegocio.textContent = testeDados["nome-negocio"];
+        }
+        if (elementoNicho !== null) {
+            elementoNicho.textContent = testeDados.nicho;
+        }
+        if (elementoModalidade !== null) 
+        {
+            elementoModalidade.textContent = capitalizarPrimeiraLetra(testeDados.modalidade); 
+        }
+
+        if (elementoSite !== null) {
+            elementoSite.textContent = testeDados.site;
+        }
+        if (elementoInstagram !== null) {
+            elementoInstagram.textContent = testeDados["perfil-instagram"];
+        }
+        if (elementoFacebook !== null) {
+            elementoFacebook.textContent = testeDados["perfil-facebook"];
+        }
+        if (elementoTelefone !== null) {
+            elementoTelefone.textContent = testeDados.telefone;
+        }
+        // Se necessário, adicione mais verificações para os outros campos
+
+    } catch (error) {
+        console.log(error);
     }
 }
+
 
 // função principal do arquivo
 function MeusDados()
 {
-     /*Gets e sets dos campos que necessitam de validão: CPF, EMAIL, Senha */
-    const [cpf, setCpf] = useState('');
-    const [email, setEmail] = useState('');
-    const [cpfValido, setCpfValido] = useState(true);
-    const [emailValido, setEmailValido] = useState(true);
-    const [senha, setSenha] = useState('');
-    const [confirmarSenha, setConfirmarSenha] = useState('');
 
-
-    // Guardando resultado em uma variável bool para que possa ser usada para mostrar
-    // a mensagem de error de digitação, caso seja false.
-    
-    useEffect(() => {
-        setCpfValido(isValidCPF(cpf));
-    }, [cpf]);
-
-    useEffect(() => {
-        setEmailValido(isValidEmail(email));
-    }, [email]);
-
-    // Funções colocando no evento Onchange para pegar os valores que estão sendo digitados em tempo real
-    const handleCpfChange = (event) => {
-        setCpf(event.target.value);
-    };
-
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    };
+   
 
     return(
             <div>
                 <div id="form">
                    
-                 
-
                     <div className="boxForm">
 
                         {/*Contém todo o formulário e seus campos*/}
-                        <form id="formContainer" onSubmit={CadastroRealizado}>
+                        <form id="formContainer">
                             <div id="formTitle1">
                                 <h1>MEUS DADOS</h1>
                             </div>
@@ -91,18 +228,19 @@ function MeusDados()
                                     {/*NOME COMPLETO*/}
                                     <div className="fieldType1">
                                         <span className="nameField">Nome Completo</span>
-                                        <span className="conteudo">Nome Completo iodvncoicnoincncncncnnnnl</span>
+                                        <span className="conteudo" id="campoNome">Nome Completo ncncnnnnl</span>
                                     </div>
 
                                     {/*DATA DE NASCIMENTO*/}
                                     <div className="fieldType1">
                                         <span className="nameField">Data de Nascimento</span>
-                                        <span className="conteudo">00/00/0000</span>
+                                        <span className="conteudo" id="campoDataNascimento">00/00/0000</span>
                                     </div> 
+                                   
                                 </div>
                                  
                                     
-                                    <img src={ProfileImg} alt="Foto de perfil" id="fotoPerfil"/>
+                                <img src={ProfileImg} alt="Foto de perfil" id="fotoPerfil"/>
                             </fieldset>
                                 
                             {/*Campo das informações pessoais*/}
@@ -112,13 +250,13 @@ function MeusDados()
                                 {/*CPF*/}
                                 <div className="fieldType1">
                                         <span className="nameField">CPF</span>
-                                        <span className="conteudo">000.000.000-00</span>
+                                        <span className="conteudo" id="campoCPF">000.000.000-00</span>
                                 </div>
 
                                  {/*Endereço completo*/}
                                 <div className="fieldType1">
                                         <span className="nameField">Endereço Completo</span>
-                                        <span className="conteudo">Rua/Avenida, nº 00 - Bairro, Cidade - UF</span>
+                                        <span className="conteudo" id="campoEndereco">Rua/Avenida, nº 00 - Bairro, Cidade - UF</span>
                                 </div>
    
                             </fieldset>
@@ -129,31 +267,31 @@ function MeusDados()
                                 {/*Nome do Negócio*/}
                                 <div className="fieldType1">
                                     <span className="nameField">Nome do negócio</span>
-                                    <span className="conteudo">Gastronomia</span>    
+                                    <span className="conteudo" id="campoNomeNegocio">Gastronomia</span>    
                                 </div>
 
                                 {/*Nicho*/}
                                 <div className="fieldType1">
                                     <span className="nameField">Nicho do Trabalho</span>
-                                    <span className="conteudo">Gastronomia</span>    
+                                    <span className="conteudo" id="campoNicho">Gastronomia</span>    
                                 </div>
                                 
                                 {/*Modalidade de Serviço*/}
                                 <div className="fieldType1">
                                     <span className="nameField">Modalidade de Serviço</span>
-                                    <span className="conteudo">Híbrido</span>    
+                                    <span className="conteudo" id="campoModalidade">Híbrido</span>    
                                 </div>
                               
                                 {/*Site do Negócio*/}
                                 <div className="fieldType1">
                                     <span className="nameField">Site do Negócio</span>
-                                    <span className="conteudo">Não possui</span>    
+                                    <span className="conteudo" id="campoSite">Não possui</span>    
                                 </div>
                                 
                                 {/*MEI*/}
                                 <div className="fieldType1">
                                     <span className="nameField">MEI</span>
-                                    <span className="conteudo">000000000000000</span>    
+                                    <span className="conteudo" id="campoMEI">000000000000000</span>    
                                 </div>
                             </fieldset>
 
@@ -161,13 +299,13 @@ function MeusDados()
                                  {/*Email Cadastrado*/}
                                  <div className="fieldType1">
                                     <span className="nameField">Email Cadastrado</span>
-                                    <span className="conteudo">algumacoisa@gmail.com</span>    
+                                    <span className="conteudo" id="campoEmail">algumacoisa@gmail.com</span>    
                                 </div>
 
                                  {/*Número de Contato*/}
                                  <div className="fieldType1">
                                     <span className="nameField">Número de Contato</span>
-                                    <span className="conteudo">+55 (81) 40028922</span>    
+                                    <span className="conteudo" id="campoTel">(81) 40028922</span>    
                                 </div>
                             </fieldset>
                             
@@ -176,19 +314,21 @@ function MeusDados()
                                     {/*Instagram*/}
                                     <div className="fieldType2 iconCenter">
                                         <img src={InstagramIcon}  alt="Icone do instagram" />
-                                        <span className="conteudo">@perfil do instagram</span>    
+                                        <span className="conteudo" id="campoInsta">@perfil do instagram</span>    
                                     </div>
 
                                         {/*Facebook*/}
                                     <div className="fieldType2 iconCenter">
                                         <img src={FacebookIcon}  alt="Icone do facebook"/>
-                                        <span className="conteudo">perfil do facebook</span>    
+                                        <span className="conteudo" id="campoFace">perfil do facebook</span>    
                                     </div>
                                     
                                 </fieldset>
 
                                 <div className="buttonsFormEdit">  
-                                        <button id="editButton">EDITAR DADOS</button>
+                                        <button id="editButton" type="button" onClick={() => {
+                                         PegandoDados(); ToggleModal();
+                                        }}>EDITAR DADOS</button>
                                 </div>
                             
                            
