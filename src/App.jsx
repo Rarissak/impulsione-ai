b20f12e0091c028dc0efe4b68e91a14189a9347b
Vinicius <viniciusvill300@gmail.com>
2024-04-27 22:04:10 -0300

    feat: criação da tela meus cartões
    quem fizer o review por favor ajustar o tamanho da pagina para ocupar todo o espeço e não ficar com aquela area vazia abaixo do footer

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import Admin from '../src/pages/Admin/index.jsx';
 import PerfilParceiro from './pages/Parceiro/perfilParceiro/index.jsx';
 // import EsqueciSenha from './pages/UsuarioComum/forgotPassword/esqueciSenha.jsx';
 import Pesquisa from './pages/UsuarioComum/Pesquisa/pesquisa.jsx';
+import MeusCartoes from './pages/UsuarioComum/meusCartoes/meusCartoes.jsx';
 import UserProfile from './components/userProfile/userProfile.jsx';
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
         <Route path='/perfilParceiro' Component={PerfilParceiro} />
 
         <Route path='/pesquisa' Component={Pesquisa} />
+        <Route path='/meusCartoes' Component={MeusCartoes} />
       </Routes>
     </Router>
   );
