216cc5e5f9d1b29bd2e96a13261c393f64b89b51
Helton Escobar <114950662+Hscobar@users.noreply.github.com>
2024-05-15 01:40:14 -0300

    Edit Feat: Puxando os dados do usuário, do back

---
+++ src/components/PerfilUsuario/perfilUsuario.jsx
@@ -0,0 +1,16 @@
+import TituloBorda from '../../../components/featured/titleBorda';
+import Footer from '../../../components/footer/footer';
+import Header from '../../../components/header/header';
+import UserProfile from '../../../components/userProfile/userProfile';
+import './perfilUsuario.css';
+
+export default function TelaPerfilUsuario()
+{
+    return(
+        <div>
+            <Header/>
+            <UserProfile/>
+            <Footer/>
+        </div>
+    );
+}
