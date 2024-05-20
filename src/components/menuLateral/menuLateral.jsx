9e3f80f3f496a7bb2bd704411da621fbb2ea8151
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-05-19 23:43:08 -0300

    edit feat: adição de código para integração e link para perfil e logout

--- src/components/menuLateral/menuLateral.jsx
+++ src/components/menuLateral/menuLateral.jsx
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import Login from '../../pages/UsuarioComum/login/login';
-import { ToggleModal } from '../../pages/UsuarioComum/login/login';
+import Login, { ToggleModal } from '../../pages/UsuarioComum/login/login';
 import IconFechar from '../../assets/iconLoginModalClose.svg';
 import LogoResponsivo from '../../assets/logoResponsivo.svg';
 import IconHome from '../../assets/iconHome.svg';
@@ -12,11 +11,22 @@ import SearchIconSvg from '../../assets/searchIcon.svg';
 import Seta from '../../assets/setaBaixo.svg';
 import IconParceiro from '../../assets/IconParceiroBranco.svg';
 import IconPerfil from '../../assets/iconPerfilBranco.svg';
+import axios  from 'axios';
+import useAxios, { useAxiosWithDependecies } from '../../hook/useAxios';
+
+import axiosInstance from '../../helper/axiosInstance';
 
 
 function MenuLateral(LoginPerfil, linkPerfil){
 
     const [expandir, setExpandir] = useState(false);
+    const token = localStorage.getItem('token')
+    const id = localStorage.getItem('id')
+    const uri = localStorage.getItem('uri')
+    const [itemPesquisa, setItemPesquisa] = useState("")
+    const [tipoUsuario, setTipoUsuario] = useState('');
+    const [rota, setRota] = useState('');
+
 
     const toggleExpanded = () => {
         setExpandir(!expandir);
@@ -34,6 +44,100 @@ function MenuLateral(LoginPerfil, linkPerfil){
     const handleComponentLogin = () => {
         ToggleModal();
     };
+
+
+    //CÓDIGO DA INTEGRAÇÃO IGUAL AO HEADER
+        const handleInputChange = (event) => {
+            setItemPesquisa(event.target.value);
+        };
+
+        const handleKeyPress = (event) => {
+            if (event.key === 'Enter') {
+                window.location.href = `/pesquisa/${itemPesquisa}`
+            }
+        };
+
+        const [isLogado, setLogado] = useState(false)
+
+        useEffect(() => {
+            if (token != null) {
+                setLogado(true);
+            }
+        }, [token]);
+
+
+        const [usuarioLogado, loading, error] = useAxiosWithDependecies({
+            axiosInstance: axiosInstance,
+            method: 'GET',
+            url: uri + '/' + id
+        }, [isLogado])
+
+        useEffect(() => {
+            const fetchUserType = async () => {
+                try {
+                    const adminResponse = await axiosInstance.get(`/admin/${id}`);
+                    if (adminResponse.data) {
+                        setTipoUsuario('admin');
+                        return;
+                    }
+                } catch (error) {
+                    console.log('Usuário não é admin', error);
+                }
+
+                try {
+                    const empreendedorResponse = await axiosInstance.get(`/empreendedores/${id}`);
+                    if (empreendedorResponse.data) {
+                        setTipoUsuario('empreendedor');
+                        return;
+                    }
+                } catch (error) {
+                    console.log('Usuário não é empreendedor', error);
+                }
+
+                try {
+                    const usuarioResponse = await axiosInstance.get(`/usuarios/${id}`);
+                    if (usuarioResponse.data) {
+                        setTipoUsuario('usuario');
+                    }
+                } catch (error) {
+                    console.log('Erro ao obter tipo de usuário', error);
+                }
+            };
+            if (isLogado) {
+                fetchUserType();
+            }
+        }, [isLogado, id]);
+
+
+        function logout() {
+            localStorage.clear();
+            window.location.href = "/";
+        }
+
+        // function perfil(){
+        //     window.location.href = "/perfilParceiro"
+        // }
+
+        const handlePerfilClick = () => {
+            let rota = '/';
+            switch (tipoUsuario) {
+                case 'admin':
+                    setRota('/adminSolicitações');
+                    break;
+                case 'empreendedor':
+                    setRota('/perfilParceiro');
+                    break;
+                case 'usuario':
+                    setRota('/perfilUsuario');
+                    break;
+                default:
+                    setRota('/');
+            }
+            navigate(rota);
+        };
+
+
+    //-------------------------------------
 
     return(
         <nav class="navResponsivo" id='espacar'>
@@ -67,20 +171,48 @@ function MenuLateral(LoginPerfil, linkPerfil){
                         </Link>
                     </li>
                     <li>
-                        <button className='botaoLogin' onClick={toggleExpanded}>
+                    {!isLogado && (
+                        <button
+                            id='headerLogin'
+                            className='centralizeItems headerScreenButtons'
+                            onClick={handleComponentLogin}>
+                            <img className='iconResponsivo' src={IconPerfil} alt='icon perfil'/>
+                            <a id='linkMenu' className='title'>Login</a>
+                        </button>)}
+                    {!isLogado && <Login />}
+
+                    {isLogado && (
+                        <button 
+                            id='headerLogin'
+                            className='botaoLogin'
+                            onClick={toggleExpanded}
+                        >
                             <div className='itemLista'>
                                 <img className='iconResponsivo' src={IconPerfil} alt='icon perfil'/>
-                                <a id='linkMenu' className='title'>Login</a>
+                                {/* <a id='linkMenu' className='title'>Login</a> */}
+                                <span id='linkMenu'>{"Olá, " + usuarioLogado.nomeExibicao}</span> 
                             </div>
-                            <img id='setaVerMais' src={Seta} alt='seta para ver mais'/>
-                        </button>
+                            <img id='setaVerMais' src={Seta} alt='seta para ver mais'/> 
+                        </button>)}
+                        
                         {expandir && (
                             <div id='loginExpandido'>
                                 {/* <button className='title' onClick={handleComponentLogin}>Perfil</button> */}
                                 {/* A FUNÇÃO ONCLICK NÃO ESTÁ PEGANDO */}
-                                <Link className='title' onClick={handleComponentLogin}>Perfil</Link>
-                                {<Login />}
-                                <Link className='title'>Sair</Link>
+                                {/* <Link 
+                                    className='title' 
+                                    onClick={handleComponentLogin}
+                                >Perfil</Link>
+                                {<Login />} */}
+                                <Link 
+                                    className='title' 
+                                    onClick={handlePerfilClick}
+                                    to={rota}
+                                >Perfil</Link>
+                                <Link 
+                                    className='title'
+                                    onClick={logout}
+                                >Sair</Link>
                             </div>
 
                         )}
